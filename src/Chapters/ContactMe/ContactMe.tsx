b8089e4a353c3f45efa24c704ddaf795b6d8ef6f
Koraiko <japan8otaku@gmail.com>
2025-07-25 01:48:30 +0200

    feat: started contact file

--- src/Chapters/ContactMe/ContactMe.tsx
+++ src/Chapters/ContactMe/ContactMe.tsx
@@ -5,7 +5,10 @@ class ContactMe extends Component {
   render() {
     return (
       <>
-        <div id='ContactComponent'>Test</div>
+        <div id='ContactComponent'>Contact-Component</div>
+        <a href="mailto:s.sabrina.marek@gmail.com" className='text-decoration-none fc-white d-flex align-items-center'>
+          <span className="m-s-filled pe-2 fs-4">email</span> s.sabrina.marek@gmail.com
+        </a>
       </>
     );
   }
