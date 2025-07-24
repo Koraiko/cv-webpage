32ca15eadbe17441220fa962a66e89a1530ec5b8
Koraiko <japan8otaku@gmail.com>
2025-07-25 01:48:52 +0200

    feat: added links to inspirations

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import Footer from './Layout/Footer';
 
 
 function App() {
+  const inspirationCvPages = [{
+    name: 'Aditya Seth',
+    url: 'https://adityaseth.in/'
+  },{
+    name: "Dr. Aditya Kumar Gupta",
+    url: 'https://aditya30051993.github.io/my-portfolio'
+  },
+  {
+    name: 'Abhishek Ganvir (TODO: use this)',
+    url: 'https://abhishekganvir.vercel.app/'
+  },{
+    name: 'Tajmirul',
+    url: 'https://www.me.toinfinite.dev/'
+  }];
   return (
     <>
       <div className="alert alert-danger alert-dismissible m-3 text-center" role="alert">
@@ -17,6 +31,18 @@ function App() {
       <CvOverview />
       <CvTimeline />
       <ContactMe />
+      <div>
+        Designed & Developed by me<br />
+        Inspirations:
+        
+        <ul>
+          {inspirationCvPages.map((inspiration, index) => (
+            <li key={index}>
+              <a href={inspiration.url} target="_blank" rel="noopener noreferrer">{inspiration.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
       <Footer />
     </>
   );
