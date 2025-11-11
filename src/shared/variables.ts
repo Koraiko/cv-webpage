7035e33d39fa0db57bee0a2bc60a310e964439f1
Koraiko <japan8otaku@gmail.com>
2025-11-11 22:46:08 +0100

    refactor: removed or updated css-rules/ files

--- src/shared/variables.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Total hours worked on projects
- * 360h RehaCat+
- * 360h Bachelor
- * => not used anymore, but kept bc of "isolatedModules compile error"
- */
-export const hoursWorked = 720;
-
