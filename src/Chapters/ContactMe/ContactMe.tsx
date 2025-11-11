6e0566a1015cd95cc00da5de0ae5336f88ce2d91
Koraiko <japan8otaku@gmail.com>
2025-11-11 22:32:10 +0100

    feat: updated contact me bootstrap classes

--- src/Chapters/ContactMe/ContactMe.tsx
+++ src/Chapters/ContactMe/ContactMe.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 const ContactMe = () => {
 
     return (
-      <>
+      <div className='bg-warning-subtle'>
         <div id='ContactComponent'>Contact-Component</div>
-        <a href="mailto:s.sabrina.marek@gmail.com" className='text-decoration-none fc-white d-flex align-items-center'>
+        <a href="mailto:s.sabrina.marek@gmail.com" className='text-decoration-none d-flex align-items-center'>
           <span className="m-s-filled pe-2 fs-4">email</span> s.sabrina.marek@gmail.com
         </a>
-      </>
+      </div>
     );
 }
 
